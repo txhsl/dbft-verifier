83e63b655b7da47af46303f858b1e51d4b12b149
Hu Shili <799498265@qq.com>
2025-05-29 11:56:53 +0800

    add n3 LC verifier sp1 template

--- n3/rust/script/src/bin/evm.rs
+++ n3/rust/script/src/bin/evm.rs
@@ -12,7 +12,7 @@
 
 use alloy_sol_types::SolType;
 use clap::{Parser, ValueEnum};
-use neox_dbft_verifier_lib::ProofOutputs;
+use fibonacci_lib::PublicValuesStruct;
 use serde::{Deserialize, Serialize};
 use sp1_sdk::{
     include_elf, HashableKey, ProverClient, SP1ProofWithPublicValues, SP1Stdin, SP1VerifyingKey,
@@ -20,16 +20,14 @@ use sp1_sdk::{
 use std::path::PathBuf;
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
-pub const VERIFIER_ELF: &[u8] = include_elf!("neox-dbft-verifier-program");
+pub const FIBONACCI_ELF: &[u8] = include_elf!("fibonacci-program");
 
 /// The arguments for the EVM command.
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct EVMArgs {
-    #[arg(long, default_value = "./script/files/block_2715865.json")]
-    parent: String,
-    #[arg(long, default_value = "./script/files/block_2715866.json")]
-    current: String,
+    #[arg(long, default_value = "20")]
+    n: u32,
     #[arg(long, value_enum, default_value = "groth16")]
     system: ProofSystem,
 }
@@ -44,12 +42,10 @@ enum ProofSystem {
 /// A fixture that can be used to test the verification of SP1 zkVM proofs inside Solidity.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct DBFTVerificationProofFixture {
-    prev_header: String,
-    new_header: String,
-    execution_state_root: String,
-    consensus_hash: String,
-    next_consensus_hash: String,
+struct SP1FibonacciProofFixture {
+    a: u32,
+    b: u32,
+    n: u32,
     vkey: String,
     public_values: String,
     proof: String,
@@ -62,27 +58,17 @@ fn main() {
     // Parse the command line arguments.
     let args = EVMArgs::parse();
 
-    // Read the parent and current block headers from the provided files.
-    let parent_header: alloy_rpc_types::Header =
-        serde_json::from_str(&std::fs::read_to_string(args.parent).unwrap()).unwrap();
-
-    let current_header: alloy_rpc_types::Header =
-        serde_json::from_str(&std::fs::read_to_string(args.current).unwrap()).unwrap();
-    println!("New block height: {}", current_header.number);
-
     // Setup the prover client.
     let client = ProverClient::from_env();
 
     // Setup the program.
-    let (pk, vk) = client.setup(VERIFIER_ELF);
+    let (pk, vk) = client.setup(FIBONACCI_ELF);
 
     // Setup the inputs.
     let mut stdin = SP1Stdin::new();
-    let encoded_1 = serde_json::to_vec(&parent_header).unwrap();
-    let encoded_2 = serde_json::to_vec(&current_header).unwrap();
-    stdin.write_vec(encoded_1);
-    stdin.write_vec(encoded_2);
+    stdin.write(&args.n);
 
+    println!("n: {}", args.n);
     println!("Proof System: {:?}", args.system);
 
     // Generate the proof based on the selected proof system.
@@ -103,21 +89,13 @@ fn create_proof_fixture(
 ) {
     // Deserialize the public values.
     let bytes = proof.public_values.as_slice();
-    let ProofOutputs {
-        prevHeader,
-        newHeader,
-        executionStateRoot,
-        consensusHash,
-        nextConsensusHash,
-    } = ProofOutputs::abi_decode(bytes).unwrap();
+    let PublicValuesStruct { n, a, b } = PublicValuesStruct::abi_decode(bytes).unwrap();
 
     // Create the testing fixture so we can test things end-to-end.
-    let fixture = DBFTVerificationProofFixture {
-        prev_header: format!("0x{}", hex::encode(prevHeader)),
-        new_header: format!("0x{}", hex::encode(newHeader)),
-        execution_state_root: format!("0x{}", hex::encode(executionStateRoot)),
-        consensus_hash: format!("0x{}", hex::encode(consensusHash)),
-        next_consensus_hash: format!("0x{}", hex::encode(nextConsensusHash)),
+    let fixture = SP1FibonacciProofFixture {
+        a,
+        b,
+        n,
         vkey: vk.bytes32().to_string(),
         public_values: format!("0x{}", hex::encode(bytes)),
         proof: format!("0x{}", hex::encode(proof.bytes())),
